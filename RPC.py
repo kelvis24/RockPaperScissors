4d53a50cd81af93fff36ac6a7ecb023f2c94c779
kelvis24 <elviskimara@outlook.com>
2020-06-02 11:21:12 -0500

    me without help

--- RPC.py
+++ RPC.py
@@ -20,6 +20,53 @@ import random, sys
                 break
         
 
+        
+        randomInput = random.randint(1,3)
+        
+        if randomInput == 1:
+            computerInput = r
+            print('Computer chose Rock')
+        elif randomInput == 2:
+           computerInput = p
+           print('Computer chose Paper')
+        elif randomInput == 3:
+           computerInput = s
+           print('Computer chose Scissors')
+        
+
+        print('                            ')
+
+        if computerInput == userInput:
+            print('it is a tie')
+            draws = draws + 1
+        elif computerInput == r && userInput == p:  
+            print('Paper beats rock so User wins')
+            win = win + 1            
+        elif computerInput == p && userInput == r:
+            print('Paper beats rock so Computer wins')
+            loss = loss + 1    
+        elif computerInput == r && userInput == s:
+            print('Rock beats Scissors so Computer wins')
+            loss = loss + 1
+        elif computerInput == s && userInput == r:
+            print('Rock beats Scissors so User wins')
+            win = win + 1
+        elif computerInput == s && userInput == p:
+            print('scissors beat paper so Computer wins')
+            loss = loss + 1
+        elif computerInput == p && userInput == s:
+            print('scissors beat paper so User wins')
+            win = win + 1
+            
+        
+
+                
+
+        print('                            ')
+        print('                            ')
+        print('                            ')
+        print('                            ')
+        print('                            ')
                 
         
         
