fcc355ab95d342f6c10eccb3c82c81be4cc84bcd
kelvis24 <elviskimara@outlook.com>
2020-06-02 01:27:58 -0500

    my first start to using python

---
+++ RPC.py
@@ -0,0 +1,42 @@
+import random, sys
+
+    
+    win = 0
+    loss = 0
+    draws = 0
+    
+    
+    while True: #the main code of the entire game
+        
+        while True: #Gets the required user output
+            print('Please enter (r)ock, (p)aper, or (s)cissor or (q)uit to play')
+            
+            userInput = input()
+            
+            if userInput == q:
+                sys.exit()
+            elif userInput == r or userInput == p or userInput = s:
+                print('User entered ' + userInput + '...')
+                break
+        
+
+                
+        
+        
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
